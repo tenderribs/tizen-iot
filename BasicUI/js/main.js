5c578806739aba9f3c667d7f7c65e5cb0a4153b8
tenderribs <dc3dude@gmail.com>
2018-12-30 15:43:40 +0100

    toggle light

--- BasicUI/js/main.js
+++ BasicUI/js/main.js
@@ -1,4 +1,3 @@
-
 window.onload = function () {
     // TODO:: Do your initialization job
 
@@ -11,10 +10,40 @@ window.onload = function () {
 	}
     });
 
-    var lightbulb = document.querySelector('.contents');
-    lightbulb.addEventListener("click", function(){
-    	light = document.querySelector('#lightbulb');
-    	light.innerHTML = light.innerHTML == "<img scr='lightoff.png'>" ? "<img scr='lighton.png'>" : "<img scr='lightoff.png'>";
+    var lightbulb = document.querySelector('#lightbulb');
+    
+    console.log('before adding click listener to lightbob');
+    
+    var isLightbulbOn = false;
+
+    lightbulb.addEventListener("click", function() {
+        console.log('lightbob clicked');
+        light = document.querySelector('#lightbulbImage');
+        
+        if (isLightbulbOn) {
+            console.log('lightbob turning off');
+            light.innerHTML = "<img src='lightoff.png'>"
+            isLightbulbOn = false;
+
+        } else {
+            console.log('lightbob turning on');
+            isLightbulbOn = true;
+            sendRequestToEpicSchool();
+            light.innerHTML = "<img src='lighton.png'>"
+        }
     });
     
 };
+
+
+function sendRequestToEpicSchool(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "https://api.pumpipumpe.ch/api/v1/product", true);
+    xhttp.send();
+
+}
